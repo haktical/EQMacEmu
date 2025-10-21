haktical/EQMacEmu

commit e71744e8f7139f6c9db4d431d6e328267dbb51de

Test commit for Partil

zone/doors.cpp
@@ -431,11 +431,11 @@ void Doors::HandleClick(Client* sender, uint8 trigger, bool floor_port)
 		}
 		else if ((!IsDoorOpen() || open_type == 58 || floor_port) && (keyneeded && ((keyneeded == playerkey) || sender->GetGM())))
 		{
-			if(!keepoffkeyring)
+			if (!keepoffkeyring)
 			{
 				sender->KeyRingAdd(playerkey);
 			}
-			if(zoneid == zone->GetZoneID())
+			if (zoneid == zone->GetZoneID())
 			{
 				sender->MovePCGuildID(zone->GetZoneID(), zoneguildid, m_destination.x, m_destination.y, m_destination.z, m_destination.w);
 			}
@@ -445,8 +445,7 @@ void Doors::HandleClick(Client* sender, uint8 trigger, bool floor_port)
 				sender->MovePCGuildID(zoneid, zoneguildid, temp_x, temp_y, m_destination.z, m_destination.w);
 			}
 		}
-
-		if ((!IsDoorOpen() || open_type == 58) && !keyneeded)
+		else if ((!IsDoorOpen() || open_type == 58) && !keyneeded)
 		{
 			if(zoneid == zone->GetZoneID())
 			{

commit 4d4f502cf0dc5db3cdb3d4efe61ff9d455b3300d

Fix VP dragon portals
Add check for same zone to first if floor_port check; if not the same zone, use zoneid instead of zone->GetZoneID.

This was previously relying on the "double zone" behavior to function and was broken by commit e71744e.

Minor refactor to remove bool is_same_zone as it was redundant with zoneid == zone->GetZoneID() checks.

zone/doors.cpp
@@ -426,8 +426,15 @@ void Doors::HandleClick(Client* sender, uint8 trigger, bool floor_port)
 			{
 				sender->KeyRingAdd(playerkey);
 			}
-			bool is_same_zone = strncmp(destination_zone_name, zone_name, strlen(zone_name)) == 0;
-			sender->MovePCGuildID(zone->GetZoneID(), is_same_zone ? zone->GetGuildID() : zoneguildid, m_destination.x, m_destination.y, m_destination.z, m_destination.w);
+			if (zoneid == zone->GetZoneID())
+			{
+				sender->MovePCGuildID(zone->GetZoneID(), zone->GetGuildID(), m_destination.x, m_destination.y, m_destination.z, m_destination.w);
+			}
+			else
+			{
+				zone->ApplyRandomLoc(zoneid, temp_x, temp_y);
+				sender->MovePCGuildID(zoneid, zoneguildid, temp_x, temp_y, m_destination.z, m_destination.w);
+			}
 		}
 		else if ((!IsDoorOpen() || open_type == 58 || floor_port) && (keyneeded && ((keyneeded == playerkey) || sender->GetGM())))
 		{
@@ -437,8 +444,7 @@ void Doors::HandleClick(Client* sender, uint8 trigger, bool floor_port)
 			}
 			if (zoneid == zone->GetZoneID())
 			{
-				bool is_same_zone = strncmp(destination_zone_name, zone_name, strlen(zone_name)) == 0;
-				sender->MovePCGuildID(zone->GetZoneID(), is_same_zone ? zone->GetGuildID() : zoneguildid, m_destination.x, m_destination.y, m_destination.z, m_destination.w);
+				sender->MovePCGuildID(zone->GetZoneID(), zone->GetGuildID(), m_destination.x, m_destination.y, m_destination.z, m_destination.w);
 			}
 			else
 			{
@@ -450,8 +456,7 @@ void Doors::HandleClick(Client* sender, uint8 trigger, bool floor_port)
 		{
 			if(zoneid == zone->GetZoneID())
 			{
-				bool is_same_zone = strncmp(destination_zone_name, zone_name, strlen(zone_name)) == 0;
-				sender->MovePCGuildID(zone->GetZoneID(), is_same_zone ? zone->GetGuildID() : zoneguildid, m_destination.x, m_destination.y, m_destination.z, m_destination.w);
+				sender->MovePCGuildID(zone->GetZoneID(), zone->GetGuildID(), m_destination.x, m_destination.y, m_destination.z, m_destination.w);
 			}
 			else
 			{